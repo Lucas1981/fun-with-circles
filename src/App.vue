fbb8013f5a4c264a836b34f860466b436ab2c37b
Lucas van Heerikhuizen <lucasvanheerikhuizen@Lucass-MacBook-Pro.local>
2020-02-29 13:21:53 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1 class="text-center">Fun With Circles</h1>
+      <div class="wrapper">
+        <canvas class="canvas" ref="canvas"></canvas>
+        <div class="card controls">
+          <div class="card-body">
+            <my-input
+              v-model="turns"
+              label="Turns"
+              type="number"
+              @input="drawPolygon"
+            />
+            <my-input
+              v-model="radius"
+              label="Radius"
+              type="number"
+              @input="drawPolygon"
+            />
+            <my-input
+              v-model="skip"
+              label="Skip"
+              type="number"
+              @input="drawPolygon"
+            />
+            <my-input
+              v-model="thickness"
+              label="Thickness"
+              type="number"
+              @input="drawPolygon"
+            />
+            <my-input
+              v-model="fillColor"
+              label="Fill color"
+              type="color"
+              @input="drawPolygon"
+            />
+            <my-input
+              v-model="strokeColor"
+              label="Stroke color"
+              type="color"
+              @input="drawPolygon"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyInput from './components/my-input.vue';
+
+const width = 800;
+const height = 512;
+const radius = 200;
+const degreesToRadians = degrees => degrees * Math.PI / 180;
+const fillColor = 'black';
+const strokeColor = 'green';
 
 export default {
   name: 'app',
+  data: () => ({
+    canvas: null,
+    ctx: null,
+    turns: 11,
+    skip: 1,
+    thickness: 3,
+    radius,
+    width,
+    height,
+    fillColor,
+    strokeColor
+  }),
   components: {
-    HelloWorld
+    MyInput
+  },
+  computed: {
+    centerX() { return this.width / 2; },
+    centerY() { return this.height / 2; },
+    turn() { return 360 / this.turns; }
+  },
+  mounted() {
+    this.canvas = this.$refs.canvas;
+    this.ctx = this.canvas.getContext('2d');
+    this.canvas.width = this.width;
+    this.canvas.height = this.height;
+    this.drawPolygon();
+  },
+  methods: {
+    drawPolygon() {
+      this.ctx.clearRect(0, 0, this.width, this.height);
+      this.ctx.strokeStyle = this.strokeColor;
+      this.ctx.fillStyle = this.fillColor;
+      this.ctx.lineWidth = this.thickness;
+      this.ctx.beginPath();
+      for (let i = 0; i < this.turns; i++) {
+        const x = Math.sin(degreesToRadians(this.turn * (i * this.skip))) * this.radius;
+        const y = Math.cos(degreesToRadians(this.turn * (i * this.skip))) * this.radius;
+        if (i === 0) this.ctx.moveTo(this.centerX + x, this.centerY + y);
+        else this.ctx.lineTo(this.centerX + x, this.centerY + y);
+      }
+      this.ctx.closePath();
+      this.ctx.fill();
+      this.ctx.stroke();
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.wrapper {
+  display: flex;
+  justify-content: center;
+  padding-top: 16px;
+  padding-bottom: 16px;
+}
+
+.canvas {
+  background-color: black;
+  width: 800px;
+}
+
+.controls {
+  margin-left: 8px;
+  width: 256px;
 }
 </style>
---
+++ src/components/my-input.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="input-group input-group-sm mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroup-sizing-sm">{{ label }}</span>
+      </div>
+      <input
+        :value="value"
+        :type="type"
+        class="form-control"
+        @change="$emit('input', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'label',
+    'type',
+    'value'
+  ]
+};
+</script>
